4ebc89008f971bb99b6032e32b4af64ab8cb9fe8
Domenico DiNicola <dom.dinicola@gmail.com>
2026-04-15 14:17:04 +0200

    fix login

--- src/hope_documents/config/urls.py
+++ src/hope_documents/config/urls.py
@@ -1,9 +1,12 @@
 from django.conf import settings
 from django.contrib.admin import site
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import include, path
 from django.views.static import serve
 
 urlpatterns = [
+    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
     path("api/", include("hope_documents.api.urls"), name="api"),
     path("favicon.ico", serve, kwargs={"document_root": settings.STATIC_ROOT, "path": "favicon.ico"}),
     path("admin/", site.urls),
