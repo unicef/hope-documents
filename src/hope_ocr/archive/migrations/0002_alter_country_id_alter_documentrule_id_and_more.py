904226e2efefdae22d7d7bd4226fce7bc9e98565
Domenico <ddinicola@unicef.org>
2026-07-10 13:04:47 +0200

    updates (#30)

--- src/hope_ocr/archive/migrations/0002_alter_country_id_alter_documentrule_id_and_more.py
+++ src/hope_ocr/archive/migrations/0002_alter_country_id_alter_documentrule_id_and_more.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("archive", "0001_initial"),
     ]
@@ -13,22 +12,16 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name="country",
             name="id",
-            field=models.AutoField(
-                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
-            ),
+            field=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID"),
         ),
         migrations.AlterField(
             model_name="documentrule",
             name="id",
-            field=models.AutoField(
-                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
-            ),
+            field=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID"),
         ),
         migrations.AlterField(
             model_name="documenttype",
             name="id",
-            field=models.AutoField(
-                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
-            ),
+            field=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID"),
         ),
     ]
